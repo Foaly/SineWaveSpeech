0556b820ba5bddc4cc33c978a116b7cf9211aa23
Maximilian Wagenbach <maximilian.wagenbach@native-instruments.de>
2016-11-05 15:09:17 +0100

    Calculate the magnitude spectrums of the sound. Also added the build script for FFTW.

---
+++ buildFFTW.sh
@@ -0,0 +1,5 @@
+# The FFTW 3 library was build with the following flags
+
+./configure  --disable-shared --disable-dependency-tracking --disable-fortran --enable-single --enable-sse
+make
+install
--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,12 @@
 #include <SFML/Graphics.hpp>
 
 #include <iostream>
+#include <iomanip>
+#include <algorithm>
 
-#include "FFT.hpp"
+#include "MagnitudeSpectrum.hpp"
 #include "ResourcePath.hpp"
+
 
 int main(int, char const**)
 {
@@ -23,6 +26,62 @@ int main(int, char const**)
 
     // Play the music
     sound.play();
+    
+    const size_t FFTSize = 512;
+    MagnitudeSpectrum magnitudeSpectrum(FFTSize);
+    
+    // get the samples as ints
+    std::vector<sf::Int16> rawSamples(soundBuffer.getSamples(), soundBuffer.getSamples() + soundBuffer.getSampleCount());
+    // make sure it can always be devided through FFTSize without remainder, if necessary add 0's
+    auto remainder = FFTSize - (rawSamples.size() % FFTSize);
+    rawSamples.insert(rawSamples.end(), remainder, 0);
+    
+    // calculate how many times the FFT will be called
+    // -1 to avoid out of bounds reading on last iteration because of our 50% sliding window
+    std::size_t numberOfRepeats = rawSamples.size() / (FFTSize / 2) - 1;
+    
+    // transform int samples into normalized float samples
+    std::vector<float> samples(rawSamples.size());
+    std::transform(rawSamples.begin(), rawSamples.end(), samples.begin(),
+                   [](float sample)
+                   {
+                       return sample / 32767.f;
+                   });
+    
+    float                           maxMagnitude = 0.f;
+    float                           minMagnitude = 0.f;
+    std::vector<std::vector<float>> magnitudes;
+    
+    auto chunckBegin = samples.cbegin();
+    for (int i = 0; i < numberOfRepeats; i++)
+    {
+        std::vector<float> sampleChunck(FFTSize);
+        std::copy(chunckBegin, chunckBegin + FFTSize, sampleChunck.begin());
+        
+        magnitudeSpectrum.process(sampleChunck);
+        
+        magnitudes.push_back(magnitudeSpectrum.getMagnitudeSpectrum());
+        
+        // normalize FFT bins
+        std::transform(magnitudes.back().begin(), magnitudes.back().end(), magnitudes.back().begin(),
+                       [](float bin)
+                       {
+                           return bin / FFTSize;
+                       });
+        
+        chunckBegin += FFTSize / 2;
+        
+        // find the max FFT bin
+        auto minmax = std::minmax_element(magnitudes.back().begin(), magnitudes.back().end());
+        // check if it's bigger than any previous one
+        if (*minmax.second > maxMagnitude)
+            maxMagnitude = *minmax.second;
+        if (*minmax.first < minMagnitude)
+            minMagnitude = *minmax.first;
+    }
+    
+    std::cout << std::fixed << std::setprecision(5);
+    std::cout << "min: " << minMagnitude << " max: " << maxMagnitude << std::endl;
 
     // Start the game loop
     while (window.isOpen())
