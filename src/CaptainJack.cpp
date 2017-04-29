b1d6e0c6a18c8b646c1a109ae2171558a9eee021
Anyere Bendrien <anyere.bendrien@posteo.de>
2017-04-29 16:15:26 +0200

    Sinegeneration for mic input

--- src/CaptainJack.cpp
+++ src/CaptainJack.cpp
@@ -18,15 +18,21 @@
 //
 ////////////////////////////////////////////////////////////
 
-#include <iostream>
-#include <stdlib.h>
-#include <unistd.h>
+#include "SineWaveSpeech.hpp"
+
 #include <jackaudioio.hpp>
 
-class PassThrough: public JackCpp::AudioIO {
+#include <stdlib.h>
+#include <algorithm>
+#include <iostream>
+#include <unistd.h>
+
+class CaptianJack: public JackCpp::AudioIO {
 
 private:
-
+    std::vector<float> m_in;
+    std::size_t m_fs;
+    SineWaveSpeech m_sineGenerator;
 
 
 public:
@@ -37,30 +43,26 @@ public:
                               // A vector of pointers to each input port.
                               audioBufVector inBufs,
                               // A vector of pointers to each output port.
-                              audioBufVector outBufs){
-
-
-        /// LOOP over all samples
-        for(int frameCNT = 0; frameCNT  < nframes; frameCNT++)
+                              audioBufVector outBufs) {
+        std::copy_n(inBufs[0], nframes, m_in.begin());
+        auto out = m_sineGenerator.generateSineWaveSpeech(m_in, m_fs);
+        for(int i = 0; i  < nframes; ++i)
         {
-
-            /// just copy sample-wise
-            outBufs[0][frameCNT] =  inBufs[0][frameCNT];
-
+            outBufs[0][i] = out[i];
         }
 
-        ///return 0 on success
         return 0;
     }
 
     /// Constructor
-    PassThrough() :
-        JackCpp::AudioIO("pass_through", 1,1){
-
-
+    CaptianJack() :
+        JackCpp::AudioIO("captian_jack", 1,1),
+        m_sineGenerator(512),
+        m_in (static_cast<std::size_t>(getBufferSize())),
+        m_fs (getSampleRate())
+    {
         reserveInPorts(1);
         reserveOutPorts(2);
-
     }
 
 };
@@ -68,40 +70,39 @@ public:
 ///
 ///
 ///
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
 
-    /// initial ports from constructor created here.
-    PassThrough t;
+    CaptianJack cj;
 
     /// activate the client
-    t.start();
+    cj.start();
 
     /// connect input
-    t.connectFromPhysical(0,0);
+    cj.connectFromPhysical(0,0);
 
     /// connect ports to stereo ports
-    t.connectToPhysical(0,0);		// connects this client out port 0 to physical destination port 0
-    t.connectToPhysical(0,1);		// connects this client out port 1 to physical destination port 1
+    cj.connectToPhysical(0,0);		// connects this client out port 0 to physical destination port 0
+    cj.connectToPhysical(0,1);		// connects this client out port 1 to physical destination port 1
 
     ///print names
     std::cout << "outport names:" << std::endl;
-    for(unsigned int i = 0; i < t.outPorts(); i++) {
-        std::cout << "\t" << t.getOutputPortName(i) << std::endl;
+    for(unsigned int i = 0; i < cj.outPorts(); i++) {
+        std::cout << "\t" << cj.getOutputPortName(i) << std::endl;
     }
 
 
     ///print names
     std::cout << "inport names:" << std::endl;
-    for(unsigned int i = 0; i < t.inPorts(); i++) {
-        std::cout << "\t" << t.getInputPortName(i) << std::endl;
+    for(unsigned int i = 0; i < cj.inPorts(); i++) {
+        std::cout << "\t" << cj.getInputPortName(i) << std::endl;
     }
 
     /// run for EVER
     sleep(-1);
 
     /// never reached:!=
-    t.disconnectInPort(0);	// Disconnecting ports.
-    t.disconnectOutPort(1);
-    t.close();	// stop client.
+    cj.disconnectInPort(0);	// Disconnecting ports.
+    cj.disconnectOutPort(1);
+    cj.close();	// stop client.
     return 0;
 }
